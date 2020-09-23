99d5bddd3b1221df61422893251b9a4e8e316654
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-09-23 12:28:38 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -9,3 +9,9 @@
 # Sample output:
 # Your name is Joe Lunchbox, and your email is joe@koolsandwiches.org.
 
+name=input("Enter your name")
+email=input("Enter your email")
+
+result=("your name is"+" "+ name+", and your email is"+" "+email+".")
+print(result)
+
